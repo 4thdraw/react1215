371fa60f774ae0f61fc3c25bbbf31c5911a3dbbc
4thdraw <4thdraw2@gmail.com>
2023-12-15 12:54:47 +0900

    소스값의 유무에 맞춰 출력하기 && 연산자 활용

--- src/Tab.js
+++ src/Tab.js
@@ -4,32 +4,43 @@ import db from './data/db.json'
 
 function Tab() {
     const [tabnum, settabnum] = useState(0);
-    // useState는 값의 변경되면 화면 컴포넌트 랜더링을 처리
-
     return (
         <section id="tabsection">
-            <div className='d-flex'>
+            <div >
+                <ul className='d-flex'>
+                    {
+                        db.tab.map((v, i) => {
+
+                            return (
+                                <li id={`tablist${i}`}>
+                                    <h3
+                                        className={`h3${i}`}
+                                        onClick={() => { settabnum(i) }}
+                                        style={{ color: tabnum === i ? "red" : null }}
+                                    >{v.tabtext}</h3>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
                 {
-                    db.tab.map((v, i) => {
-                        const colorvar = tabnum === i ? "red" : null;
-                        // colorvar의 값은 red 이거나 null로 세팅
-                        // i는 맵실행을 구분하는 역활 key값에 넣어서 인식했던 이유가 똑같음
-                        return (
-                            <>
-                                <h3
-                                    className={`h3${i}`}
-                                    onClick={() => { settabnum(i) }}
-                                    style={{ color: colorvar }}
-                                >{v.tabtext}</h3>
-                                {
-                                    //하위맵
-                                    v.tabul.map((vv, ii) => {
-                                        return ii + "번의 데이터는 " + vv.atext
-                                    })
-                                }
-                            </>
-                        )
-                    })
+                    db.tab[tabnum] &&
+                    <div>
+                        <ul>
+                            {
+
+                                db.tab[tabnum].tabul.map((vv, ii) => <li><a href={vv.href}>
+                                    {vv.atext}
+                                    {
+                                        vv.imgsrc && <img src={vv.imgsrc}></img>
+                                    }
+
+                                </a></li>)
+
+                            }
+
+                        </ul>
+                    </div>
                 }
             </div>
         </section >
